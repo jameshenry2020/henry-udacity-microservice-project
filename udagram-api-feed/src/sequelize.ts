83f63ca07a2a9ea199330ff0b8f7c579b2403568
henry <aengrhenry@gmail.com>
2022-07-11 14:31:40 -0700

    added postgres port

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -7,7 +7,7 @@ export const sequelize = new Sequelize({
   'password': config.password,
   'database': config.database,
   'host': config.host,
-
+  'port': config.port,
   'dialect': config.dialect,
   'storage': ':memory:',
 });
